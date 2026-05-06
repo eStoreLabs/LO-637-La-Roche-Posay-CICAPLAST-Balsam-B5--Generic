2a8b66d5e87ea2a1f84cf24d11447f091e71a580
Roman Pavlenko <rpavlenko.mail@gmail.com>
2026-05-06 23:06:23 +0200

    feat: add markup ad initial styles

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+(function () {
+  const root = document.querySelector('#es-rc-content');
+  if (!root) return;
+
+  const accTab = root.querySelectorAll('.es-acc-tab');
+  for (let i = 0; i < accTab.length; i++) {
+    accTab[i].addEventListener('click', function () {
+      this.classList.add('es-active');
+      let accTabContent = this.nextElementSibling;
+
+      const hideContent = () => {
+        accTabContent.style.maxHeight = null;
+        this.classList.remove('es-active');
+      };
+
+      const showContent = () => {
+        accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
+        this.classList.add('es-active');
+      };
+
+      if (accTabContent.style.maxHeight) {
+        hideContent();
+      } else {
+        showContent();
+      }
+    });
+  }
+
+  const swiper = new Swiper(root.querySelector('.es-swiper1'), {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: root.querySelector('.es-swiper1-pagination'),
+    },
+    navigation: {
+      nextEl: root.querySelector('.es-swiper1-button-next'),
+      prevEl: root.querySelector('.es-swiper1-button-prev'),
+    },
+  });
+})();
